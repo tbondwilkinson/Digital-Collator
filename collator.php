de228848c58f99f4a51348e33335c0ac4928f297
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-29 22:42:03 -0500

    t

--- collator.php
+++ collator.php
@@ -26,7 +26,7 @@
 
 			var ctx = leftCanvas.getContext("2d");
 			var img = new Image();
-			img.src = "~/SC179_BoD_1/SC179_Bod_1_A1.jpg";
+			img.src = "../../SC179_BoD_1/SC179_Bod_1_A1.jpg";
 			ctx.drawImage(img, 0, 0);
 		}
 	</script>
