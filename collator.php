464c19f86f7812e3e4d624439bccaa98187fc6dd
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-29 23:04:31 -0500

    t

--- collator.php
+++ collator.php
@@ -72,11 +72,12 @@
 			var leftImg = new Image();
 			leftImg.src = "../SC179_BoD_1/SC179_Bod_1_A1.jpg";
 			leftCtx.drawImage(leftImg, 0, 0, 500, 500);
+			alert(leftImg);
 
 			var rightImg = new Image();
 			rightImg.src = "../SC179_HRH_1/SC179_HRH_1_A1.jpg";
 			rightCtx.drawImage(rightImg, 0, 0, 500, 500);
-
+			alert(rightImg);
 		}
 	</script>
 </head>
