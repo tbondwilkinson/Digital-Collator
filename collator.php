362fe6b3709d3e7cd75c5c1d8d6fbc199f353c8d
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-29 21:49:30 -0500

    t

---
+++ collator.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<head>
+	<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/start/jquery-ui.css" type="text/css" rel="Stylesheet" />
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.0/jquery-ui.min.js"></script>
+</head>
+<body>
+	<canvas id="leftCanvas" width="200" height="100"
+	style="border:1px solid #000000; float: left">
+	</canvas>
+	<canvas id="rightCanvas" width="200" height="100"
+	style="border:1px solid #000000; float: right">
+	</canvas>
+	<div id="images" style="display:none">
+		<img id="image1" src="http://www.w3schools.com/html/img_the_scream.jpg">
+		<img id="image2" src="http://www.w3schools.com/html/img_the_scream.jpg">
+	</div>
+	<script>
+		var c, ctx, img;
+		c = $("#leftCanvas");
+		ctx = c.getContext("2d");
+		img = $("#image1");
+		ctx.drawImage(img, 0, 0);
+		alert(img.attr("height"));
+		alert(img.attr("width"));
+	</script>
+</body>
+
