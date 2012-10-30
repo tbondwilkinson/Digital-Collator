8497bb47d50b940a699a1586b357a1d76647629e
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-29 22:07:31 -0500

    t

--- collator.php
+++ collator.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<html>
 <head>
 	<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/start/jquery-ui.css" type="text/css" rel="Stylesheet" />
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
@@ -9,9 +10,7 @@
 			var c=document.getElementById("leftCanvas");
 			var ctx=c.getContext("2d");
 			var img=document.getElementById("image1");
-			ctx.drawImage(img,10,10);
-			alert(img.getAttribute("height"));
-			alert(img.getAttribute("width"));
+			ctx.drawImage(img, 0, 0);
 		}
 
 		$(document).ready(ready);
@@ -29,4 +28,5 @@
 		<img id="image2" src="http://www.w3schools.com/html/img_the_scream.jpg">
 	</div>
 </body>
+</html>
 
