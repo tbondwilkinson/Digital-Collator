b57a7c82324fa779cb82652f984ffaad556d4cee
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-29 21:56:20 -0500

    t

--- collator.php
+++ collator.php
@@ -3,19 +3,11 @@
 	<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/start/jquery-ui.css" type="text/css" rel="Stylesheet" />
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.0/jquery-ui.min.js"></script>
-</head>
-<body>
-	<canvas id="leftCanvas" width="200" height="100"
-	style="border:1px solid #000000; float: left">
-	</canvas>
-	<canvas id="rightCanvas" width="200" height="100"
-	style="border:1px solid #000000; float: right">
-	</canvas>
-	<div id="images" style="display:none">
-		<img id="image1" src="http://www.w3schools.com/html/img_the_scream.jpg">
-		<img id="image2" src="http://www.w3schools.com/html/img_the_scream.jpg">
-	</div>
+
+	
 	<script type='text/javascript'>
+		alert("It's working");
+
 		function ready() {
 			var c, ctx, img;
 			c = $("#leftCanvas");
@@ -28,5 +20,17 @@
 
 		$(document).ready(ready);
 	</script>
+</head>
+<body>
+	<canvas id="leftCanvas" width="200" height="100"
+	style="border:1px solid #000000; float: left">
+	</canvas>
+	<canvas id="rightCanvas" width="200" height="100"
+	style="border:1px solid #000000; float: right">
+	</canvas>
+	<div id="images" style="display:none">
+		<img id="image1" src="http://www.w3schools.com/html/img_the_scream.jpg">
+		<img id="image2" src="http://www.w3schools.com/html/img_the_scream.jpg">
+	</div>
 </body>
 
