2fda46de3721ce3bc490a414984d1da0c6436998
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-29 21:59:55 -0500

    t

--- collator.php
+++ collator.php
@@ -3,23 +3,6 @@
 	<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/start/jquery-ui.css" type="text/css" rel="Stylesheet" />
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.0/jquery-ui.min.js"></script>
-
-	
-	<script type='text/javascript'>
-		alert("It's working");
-
-		function ready() {
-			var c, ctx, img;
-			c = $("#leftCanvas");
-			ctx = c.getContext("2d");
-			img = $("#image1");
-			ctx.drawImage(img, 0, 0);
-			alert(img.attr("height"));
-			alert(img.attr("width"));
-		}
-
-		$(document).ready(ready);
-	</script>
 </head>
 <body>
 	<canvas id="leftCanvas" width="200" height="100"
@@ -32,5 +15,18 @@
 		<img id="image1" src="http://www.w3schools.com/html/img_the_scream.jpg">
 		<img id="image2" src="http://www.w3schools.com/html/img_the_scream.jpg">
 	</div>
+	<script type='text/javascript'>
+		function ready() {
+			var c, ctx, img;
+			c = $("#leftCanvas")[0];
+			ctx = c.getContext("2d");
+			img = $("#image1");
+			ctx.drawImage(img, 0, 0);
+			alert(img.attr("height"));
+			alert(img.attr("width"));
+		}
+
+		$(document).ready(ready);
+	</script>
 </body>
 
