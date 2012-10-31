12b314dae5b691214d92197f8955de6026046fb5
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-31 12:10:30 -0500

    t

--- collator.php
+++ collator.php
@@ -4,11 +4,10 @@
 	<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/start/jquery-ui.css" type="text/css" rel="Stylesheet" />
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.0/jquery-ui.min.js"></script>
+	<script src="jcanvas.min.js"></script>
 
 	<script type='text/javascript'>
 	var lCtx, rCtx;
-	var leftRestorePoints = [];
-	var rightRestorePoints = [];
 
 	function leftCanvasClick(e) {
 		if (e != null) {
@@ -25,10 +24,6 @@
 			} 
 			x -= leftCanvas.offsetLeft;
 			y -= leftCanvas.offsetTop;
-
-
-			var imgSrc = leftCanvas.toDataURL("image/png");
-			leftRestorePoints.push(imgSrc);
 
 			lCtx.moveTo(x - 5, y);
 			lCtx.lineTo(x + 5, y);
@@ -55,10 +50,6 @@
 			x -= rightCanvas.offsetLeft;
 			y -= rightCanvas.offsetTop;
 
-
-			var imgSrc = rightCanvas.toDataURL("image/png");
-			rightRestorePoints.push(imgSrc);
-
 			rCtx.moveTo(x - 5, y);
 			rCtx.lineTo(x + 5, y);
 			rCtx.stroke();
@@ -66,30 +57,6 @@
 			rCtx.lineTo(x, y + 5);
 			rCtx.stroke();
 		}
-	}
-
-	function keydown(event) {
-		var keyCode = ('which' in event) ? event.which : event.keyCode;
-		if (keyCode === 85) {
-			// If we have some restore points
-			if (leftRestorePoints.length > 0) {
-				var lImg = new Image();
-				lImg.onload = function() {
-					lCtx.drawImage(lImg, 0, 0);
-				}
-				lImg.src = leftRestorePoints.pop();
-			}
-			// If we have some restore points
-			if (rightRestorePoints.length > 0) {
-				var rImg = new Image();
-				rImg.onload = function() {
-					rCtx.drawImage(rImg, 0, 0);
-				}
-				rImg.src = rightRestorePoints.pop();
-			}
-			return false;
-		}
-		return true;
 	}
 
 	window.onload = function() {
@@ -100,12 +67,30 @@
 		leftCanvas.addEventListener("click", leftCanvasClick, false);
 		rightCanvas.addEventListener("click", rightCanvasClick, false);
 
-		document.onkeydown = keydown;
-
 		var leftImg = new Image();
 		leftImg.src = "http://ec2-54-245-10-30.us-west-2.compute.amazonaws.com/~tbondwilkinson/SC179_BoD_1/SC179_Bod_1_A1.jpg";
     	leftImg.onload = function() {
-			lCtx.drawImage(leftImg, 0, 0, 587, 802);
+    		$("#leftCanvas").drawImage({
+    			layer: true,
+    			source: "http://ec2-54-245-10-30.us-west-2.compute.amazonaws.com/~tbondwilkinson/SC179_BoD_1/SC179_Bod_1_A1.jpg",
+    			x: 0,
+    			y: 0,
+    			scale: 1
+    			click: function(layer) {
+    				$("canvas").drawLine({
+    				  strokeStyle: "#000",
+    				  strokeWidth: 1,
+    				  x1: layer.mouseX - 5, y1: layer.mouseY,
+    				  x2: layer.mouseX + 5, y2: layer.mouseY
+    				});
+    				$("canvas").drawLine({
+    				  strokeStyle: "#000",
+    				  strokeWidth: 1,
+    				  x1: layer.mouseX, y1: layer.mouseY - 5,
+    				  x2: layer.mouseX, y2: layer.mouseY + 5
+    				});
+    			 }
+    		});
     	};
 
 		var rightImg = new Image();
