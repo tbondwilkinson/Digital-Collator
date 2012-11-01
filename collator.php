a23ee9e9acc045d8e29a10d208d1fa99e4860a48
tbondwilkinson <tbondwilkinson@gmail.com>
2012-11-01 13:54:31 -0500

    t

--- collator.php
+++ collator.php
@@ -28,6 +28,44 @@
 			leftImages.push("http://ec2-54-245-10-30.us-west-2.compute.amazonaws.com/~tbondwilkinson/"
 						+ json.folder + this.src);
 		});
+
+		var leftCanvas = document.getElementById("leftCanvas");
+		var leftImg = new Image();
+		leftImg.src = leftFolder + leftImages.pop();
+    	leftImg.onload = function() {
+    		leftCanvas.width = window.innerWidth/2 - 2;
+    		var scale = leftCanvas.width / leftImg.width;
+    		leftCanvas.height = leftImg.height * scale;
+    		$("#leftCanvas").drawImage({
+    			layer: true,
+    			source: leftImg.src,
+    			x: 0,
+    			y: 0,
+    			fromCenter: false,
+    			width: leftCanvas.width,
+    			height: leftCanvas.height,
+    			click: function(layer) {
+    				var point = new Object();
+    				point.x = Number(Math.round(layer.mouseX / scale));
+    				point.y = Number(Math.round(layer.mouseY / scale));
+    				leftLandmarks[leftLandmarks.length] = point;
+    				$("#leftCanvas").drawLine({
+    					layer: true,
+    				  	strokeStyle: "red",
+    				  	strokeWidth: 1,
+    				  	x1: layer.mouseX - 5, y1: layer.mouseY,
+    				  	x2: layer.mouseX + 5, y2: layer.mouseY
+    				});
+    				$("#leftCanvas").drawLine({
+    					layer: true,
+    				  	strokeStyle: "red",
+    				  	strokeWidth: 1,
+    				  	x1: layer.mouseX, y1: layer.mouseY - 5,
+    				  	x2: layer.mouseX, y2: layer.mouseY + 5
+    				});
+    			 }
+    		});
+    	};
 	}
 
 	function getRightImagesCallback(event) {
@@ -43,10 +81,49 @@
 			rightImages.push("http://ec2-54-245-10-30.us-west-2.compute.amazonaws.com/~tbondwilkinson/"
 						+ json.folder + this.src);
 		});
+
+		var rightCanvas = document.getElementById("rightCanvas");
+
+		var rightImg = new Image();
+		rightImg.src = rightFolder + rightImages.pop();
+    	rightImg.onload = function() {	
+			rightCanvas.width = window.innerWidth/2 - 2;
+    		var scale = rightCanvas.width / rightImg.width;
+    		rightCanvas.height = rightImg.height * scale;
+    		$("#rightCanvas").drawImage({
+    			layer: true,
+    			source: rightImg.src,
+    			x: 0,
+    			y: 0,
+    			fromCenter: false,
+    			width: rightCanvas.width,
+    			height: rightCanvas.height,
+    			click: function(layer) {
+	    			var point = new Object();
+	    			point.x = Number(Math.round(layer.mouseX / scale));
+	    			point.y = Number(Math.round(layer.mouseY / scale));
+	    			rightLandmarks[rightLandmarks.length] = point;
+    				$("#rightCanvas").drawLine({
+    					layer: true,
+    				  	strokeStyle: "red",
+    				  	strokeWidth: 1,
+    				  	x1: layer.mouseX - 5, y1: layer.mouseY,
+    				  	x2: layer.mouseX + 5, y2: layer.mouseY
+    				});
+    				$("#rightCanvas").drawLine({
+    					layer: true,
+    				  	strokeStyle: "red",
+    				  	strokeWidth: 1,
+    				  	x1: layer.mouseX, y1: layer.mouseY - 5,
+    				  	x2: layer.mouseX, y2: layer.mouseY + 5
+    				});
+    			 }
+    		});
+    	};
 	}
 
 	function nextImage(event) {
-		
+
 
 		$("#leftCanvas").clearCanvas();
 		$("#rightCanvas").clearCanvas();
@@ -136,87 +213,6 @@
 		xmlHttp.open("GET", "getImages.php?folder=" + rightFolder);
 		xmlHttp.addEventListener("load", getRightImagesCallback, false);
 		xmlHttp.send(null);
-
-
-		var leftCanvas = document.getElementById("leftCanvas");
-		var rightCanvas = document.getElementById("rightCanvas");
-
-		// Display the first images on the two canvases.  Upon clicks, load the next image.
-		// Once all of the images have been landmarked, send an ajax request to the server with
-		// the landmark files.
-		var leftImg = new Image();
-		leftImg.src = leftFolder + leftImages.pop();
-    	leftImg.onload = function() {
-    		leftCanvas.width = window.innerWidth/2 - 2;
-    		var scale = leftCanvas.width / leftImg.width;
-    		leftCanvas.height = leftImg.height * scale;
-    		$("#leftCanvas").drawImage({
-    			layer: true,
-    			source: leftImg.src,
-    			x: 0,
-    			y: 0,
-    			fromCenter: false,
-    			width: leftCanvas.width,
-    			height: leftCanvas.height,
-    			click: function(layer) {
-    				var point = new Object();
-    				point.x = Number(Math.round(layer.mouseX / scale));
-    				point.y = Number(Math.round(layer.mouseY / scale));
-    				leftLandmarks[leftLandmarks.length] = point;
-    				$("#leftCanvas").drawLine({
-    					layer: true,
-    				  	strokeStyle: "red",
-    				  	strokeWidth: 1,
-    				  	x1: layer.mouseX - 5, y1: layer.mouseY,
-    				  	x2: layer.mouseX + 5, y2: layer.mouseY
-    				});
-    				$("#leftCanvas").drawLine({
-    					layer: true,
-    				  	strokeStyle: "red",
-    				  	strokeWidth: 1,
-    				  	x1: layer.mouseX, y1: layer.mouseY - 5,
-    				  	x2: layer.mouseX, y2: layer.mouseY + 5
-    				});
-    			 }
-    		});
-    	};
-
-		var rightImg = new Image();
-		rightImg.src = rightFolder + rightImages.pop();
-    	rightImg.onload = function() {	
-			rightCanvas.width = window.innerWidth/2 - 2;
-    		var scale = rightCanvas.width / rightImg.width;
-    		rightCanvas.height = rightImg.height * scale;
-    		$("#rightCanvas").drawImage({
-    			layer: true,
-    			source: rightImg.src,
-    			x: 0,
-    			y: 0,
-    			fromCenter: false,
-    			width: rightCanvas.width,
-    			height: rightCanvas.height,
-    			click: function(layer) {
-	    			var point = new Object();
-	    			point.x = Number(Math.round(layer.mouseX / scale));
-	    			point.y = Number(Math.round(layer.mouseY / scale));
-	    			rightLandmarks[rightLandmarks.length] = point;
-    				$("#rightCanvas").drawLine({
-    					layer: true,
-    				  	strokeStyle: "red",
-    				  	strokeWidth: 1,
-    				  	x1: layer.mouseX - 5, y1: layer.mouseY,
-    				  	x2: layer.mouseX + 5, y2: layer.mouseY
-    				});
-    				$("#rightCanvas").drawLine({
-    					layer: true,
-    				  	strokeStyle: "red",
-    				  	strokeWidth: 1,
-    				  	x1: layer.mouseX, y1: layer.mouseY - 5,
-    				  	x2: layer.mouseX, y2: layer.mouseY + 5
-    				});
-    			 }
-    		});
-    	};
 
     	document.onkeypress(nextImage);
     };
