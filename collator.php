c73dc31e012565ff1d95b6c72fbbbab32e015dbd
tbondwilkinson <tbondwilkinson@gmail.com>
2012-11-01 16:23:23 -0500

    t

--- collator.php
+++ collator.php
@@ -218,7 +218,7 @@
 		xmlHttp.addEventListener("load", getRightImagesCallback, false);
 		xmlHttp.send(null);
 
-    	document.onkeypress = nextImage;
+    	window.onkeypress = nextImage;
     };
 	</script>
 
