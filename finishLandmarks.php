e9c35cd8dcb4444846535c3e845ff2134256dd63
tbondwilkinson <tbondwilkinson@gmail.com>
2012-11-02 02:29:45 -0500

    t

--- finishLandmarks.php
+++ finishLandmarks.php
@@ -3,16 +3,15 @@
 $leftLandmarks = json_decode($_POST['leftLandmarks']);
 $rightLandmarks = json_decode($_POST['rightLandmarks']);
 
-print_r($leftLandmarks);
-
 for($i = 0; $i < count($leftLandmarks); $i++) {
 	$scanName = "landmarkFiles/" . substr_replace($leftLandmarks[$i][0]->name, ".txt", -4, -1);
 	$file = fopen($scanName, 'w') or die("Cannot open file");
 	$topOfFile = "Index\txSource\tySource\txTarget\tyTarget";
 	fwrite($file, $topOfFile);
 	for ($j = 0; $j < count($leftLandmarks[$i]); $j++) {
-		fwrite($file, $j . "\t" . $leftLandmarks[$i][$j]->x . "\t" . $leftLandmarks[$i][$j]->y . 
-			"\t" . $rightLandmarks[$i][$j]->x . "\t" $leftLandmarks[$i][$j]->y);
+		$landmark = $j . "\t" . $leftLandmarks[$i][$j]->x . "\t" . $leftLandmarks[$i][$j]->y . 
+			"\t" . $rightLandmarks[$i][$j]->x . "\t" . $leftLandmarks[$i][$j]->y;
+		fwrite($file, $landmark);
 		fclose($file);
 	}
 }
